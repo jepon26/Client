f77f0af98ceed77622842171e776cf65d8dc7a82
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-10-07 16:37:07 -0600

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home"
 
 function App() {
   return (
+    <Router>
   <div className="max-w-screen-md max-auto pt-20">
-      <Home/>
-     
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+     </Routes>
         </div>
+        </Router>
  );
 }
+
 export default App;
