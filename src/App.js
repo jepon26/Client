77f5152f1abaadb901f468f383d7ddda504ad415
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-10-08 20:52:44 -0600

    update navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import About from "./pages/About";
 import ArticlesList from "./pages/ArticlesList";
 import Article from "./pages/Article";
 
+//components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Router>
+      <Navbar/>
   <div className="max-w-screen-md max-auto pt-20">
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/articles-list" element={<ArticlesList/>}/>
       <Route path="/article" element={<Article/>}/>
