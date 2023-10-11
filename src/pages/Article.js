49da07e310cbca5e848d61114312e536cc9c0027
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-10-10 20:44:42 -0600

    update article

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import articleContent from './article-content'
 
 const Article = () => {
   const {name} = useParams ()
+  const article = articleContent.find((article) => article.name === name);
   return (
     <div>
-        <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">This is the {name} article</h1>
+        <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">{article.title}</h1>
         
         </div>
   )
