6ddfc27a6971bf917dec6c9bd3660aaee5321fa3
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-10-11 20:00:06 -0600

    update img

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Article from './Article'
 
 const ArticlesList = () => {
   return (
     <div>
         <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">Articles</h1>
-        
+        <div className='flex flex-wrap -m-4'>
+          {articleContent.map((article, index)=> (
+          <div key={index} className='p-4 md:w-1/2'>
+<div className='h-full border-2 border-gray-200 border-opacity-60 roundend-lg overflow-hidden'>
+<Link to={`/article/${article.name}`}>
+ <img className='lg:h-48 md:h-36 w-full object-cover object-center' src={article.thumbnail}></img>
+</Link>
+</div>
+          </div>
+          ))}
+          </div>div>
         </div>
   )
 }
