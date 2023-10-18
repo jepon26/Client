392424d653ec6c0b9e4d344d5615b5bd403b0646
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-10-17 20:57:58 -0600

    article name

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -21,7 +21,7 @@ const ArticlesList = () => {
 <p className='leading-relaxed mb-3 '>
 {article.content[0].substring(0, 110)}...
 </p>
-<div className='text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0'>
+<div className='text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0' to={`/article/${article.name}`}>
   Learn more
   </div>/
 </div>
